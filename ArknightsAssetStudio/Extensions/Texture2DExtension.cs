0721c649b3497e9d332273d2968d83b1c14ed314
Aloento <11802769+soarteam@users.noreply.github.com>
2021-08-07 10:56:09 +0200

    Header

--- ArknightsAssetStudio/Extensions/Texture2DExtension.cs
+++ ArknightsAssetStudio/Extensions/Texture2DExtension.cs
@@ -8,20 +8,20 @@ namespace SoarCraft.QYun.ArknightsAssetStudio.Extensions {
     using SoarCraft.QYun.TextureDecoderNET;
 
     public static class Texture2DExtension {
-        public static Image ConvertToImage(this Texture2D m_Texture2D, bool flip) {
-            _ = new ETC1Decoder(m_Texture2D.image_data.GetData(), (ulong)m_Texture2D.m_Width, (ulong)m_Texture2D.m_Height, out var bytes);
+        public static Image ConvertToImage(this Texture2D texture2D, bool flip) {
+            _ = new ETC1Decoder(texture2D.image_data.GetData(), (ulong)texture2D.m_Width, (ulong)texture2D.m_Height, out var bytes);
             if (bytes is { Length: > 0 }) {
-                var image = Image.LoadPixelData<Bgra32>(bytes, m_Texture2D.m_Width, m_Texture2D.m_Height);
-                if (flip) {
+                var image = Image.LoadPixelData<Bgra32>(bytes, texture2D.m_Width, texture2D.m_Height);
+                if (flip)
                     image.Mutate(x => x.Flip(FlipMode.Vertical));
-                }
+
                 return image;
             }
             return null;
         }
 
-        public static MemoryStream ConvertToStream(this Texture2D m_Texture2D, ImageFormat imageFormat, bool flip) {
-            var image = ConvertToImage(m_Texture2D, flip);
+        public static MemoryStream ConvertToStream(this Texture2D texture2D, ImageFormat imageFormat, bool flip) {
+            var image = ConvertToImage(texture2D, flip);
             if (image != null) {
                 using (image) {
                     return image.ConvertToStream(imageFormat);
--- ArknightsAssetStudio/Views/ShellPage.xaml.cs
+++ ArknightsAssetStudio/Views/ShellPage.xaml.cs
@@ -5,17 +5,22 @@ namespace SoarCraft.QYun.ArknightsAssetStudio.Views {
     using Microsoft.UI.Xaml;
     using Microsoft.UI.Xaml.Controls;
     using Microsoft.UI.Xaml.Input;
+    using Services;
     using ViewModels;
 
     // TODO WTS: Change the icons and titles for all NavigationViewItems in ShellPage.xaml.
     public sealed partial class ShellPage : Page {
-        private readonly KeyboardAccelerator _altLeftKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu);
+        private readonly KeyboardAccelerator _altLeftKeyboardAccelerator =
+            BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu);
+
         private readonly KeyboardAccelerator _backKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.GoBack);
 
         public ShellViewModel ViewModel { get; }
+        public SettingsService Settings { get; }
 
         public ShellPage(ShellViewModel viewModel) {
             this.ViewModel = viewModel;
+            Settings = Ioc.Default.GetRequiredService<SettingsService>();
             this.InitializeComponent();
             this.ViewModel.NavigationService.Frame = this.shellFrame;
             this.ViewModel.NavigationViewService.Initialize(this.navigationView);
@@ -28,7 +33,8 @@ namespace SoarCraft.QYun.ArknightsAssetStudio.Views {
             this.KeyboardAccelerators.Add(this._backKeyboardAccelerator);
         }
 
-        private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null) {
+        private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key,
+            VirtualKeyModifiers? modifiers = null) {
             var keyboardAccelerator = new KeyboardAccelerator { Key = key };
             if (modifiers.HasValue) {
                 keyboardAccelerator.Modifiers = modifiers.Value;
@@ -38,10 +44,16 @@ namespace SoarCraft.QYun.ArknightsAssetStudio.Views {
             return keyboardAccelerator;
         }
 
-        private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args) {
+        private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender,
+            KeyboardAcceleratorInvokedEventArgs args) {
             var navigationService = Ioc.Default.GetService<INavigationService>();
             var result = navigationService.GoBack();
             args.Handled = result;
         }
+
+        private void DarkLightModeToggle_OnClick(object sender, RoutedEventArgs e) =>
+            this.Settings.SwitchThemeCommand.Execute(this.DarkLightModeToggle.IsChecked ?? false
+                ? ElementTheme.Dark
+                : ElementTheme.Light);
     }
 }
